8afd96bfe02d0d7053ee300aaafe94d346d1c332
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2025-06-29 01:13:33 +0600

    feat: implement logout two application both

--- routes/auth.php
+++ routes/auth.php
@@ -26,4 +26,7 @@ Route::middleware('guest')->group(function () {
 Route::middleware('auth')->group(function () {
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
+
+    Route::post('client/logout', [ClientController::class, 'destroyWithRedirect'])
+        ->name('client.logout');
 });
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', DashboardController::class)->name('dashboard');
